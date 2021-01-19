608b0e38f82b9e610a153cb680f9819eaaeef8b6
Max Hauser <hauser.m97@gmail.com>
2021-01-19 20:16:03 +0100

    fixed missing files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <nav>
+    <div id="nav">
+      <!-- 
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/">Uitloggen</router-link>
+      -->
+    </div>    
+  </nav>
+  <router-view/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
@@ -21,6 +24,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
